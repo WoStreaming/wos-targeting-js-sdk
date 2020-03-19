b0a794575fdfbd54bcd7f0604478eb4a793b2d58
Justin Warkentin <jwarkentin@wideorbit.com>
2020-03-19 16:53:29 -0700

    CLARITY-5954: Release scripts

---
+++ scripts/do-release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export RELEASE_DIR="wos-targeting-sdk_release_master"
+
+npm run add-github-remote \
+  && npm run build \
+  && npm run release:clean \
+  && npm run release:get-master \
+  && cd "/tmp/$RELEASE_DIR" \
+  && npm run release:bump-version \
+  && npm run release:build \
+  && npm run release:push \
+  && npm run release:publish \
+  && npm run release:clean
--- src/dev-server/index.js
+++ src/dev-server/index.js
@@ -1,4 +1,3 @@
-import path from "path";
 import express from "express";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
@@ -8,16 +7,8 @@ import webpackConfig from "../../webpack.config.babel";
 const compiler = webpack(webpackConfig);
 
 const app = express();
-app.use(
-	webpackDevMiddleware(compiler, {
-		logTime: true
-		// publicPath: "/__webpack_hmr"
-		// writeToDisk: true
-	})
-);
+app.use(webpackDevMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
-
-// app.use(express.static("dist"));
 
 app.listen(3000, () => {
 	console.log(`Server listening on port 3000`);
