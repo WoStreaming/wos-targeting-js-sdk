0db2fcb26e1b60fead3566631cfb4ec852efffca
Jere Jones <jjones@wideorbit.com>
2022-04-04 19:15:27 -0400

    ADS-226 Remove lotame references

--- src/client/WOSTargetingClient.js
+++ src/client/WOSTargetingClient.js
@@ -1,37 +1,26 @@
-import WOSTargetingParams from "./WOSTargetingParams";
-import { addScript } from "./utils";
-
-addScript("https://tags.crwdcntrl.net/c/6395/cc_af.js");
+import WOSTargetingParams from './WOSTargetingParams';
 
 export default class WOSTargetingClient {
-	clientId = "0";
+	clientId = '0';
 	hasPrivacyPolicy = false;
 	useTestProfile = false;
 
-	constructor(clientId, hasPrivacyPolicy) {
+	constructor (clientId, hasPrivacyPolicy) {
 		this.clientId = clientId;
 		this.hasPrivacyPolicy = hasPrivacyPolicy;
 	}
 
-	getTargetingParams() {
+	getTargetingParams () {
 		const makeParams = (profile = {}) => new WOSTargetingParams(profile, { privacypolicy: this.hasPrivacyPolicy });
-		return this.getAudienceInfo()
-			.then(makeParams)
-			.catch((err) => (console.error(`Error getting targeting params: ${err}`), makeParams()));
+		return this.getAudienceInfo().
+			then(makeParams).
+			catch((err) => (console.error(`Error getting targeting params: ${err}`), makeParams()));
 	}
 
-	getAudienceInfo() {
-		// NOTE: Awkward as it may be, this has to use a JSONP request to get audience data since a normal
-		//       XHR request blocks 3rd-party cookies from being sent
+	getAudienceInfo () {
+		// This used to call Lotame to get audience data.  The side effects should be cleaned out by someone competent that can actually exercise the code
 		return new Promise((res, rej) => {
-			const cbName = `wost${(Math.random() * 1000) | 0}`;
-			const audLoadScript = addScript(`https://bcp.crwdcntrl.net/5/c=${this.clientId}/pe=y/callback=${cbName}`);
-
-			window[cbName] = ({ Profile: profile }) => {
-				delete window[cbName];
-				document.head.removeChild(audLoadScript);
-				res(profile);
-			};
+			res({});
 		});
 	}
 }
